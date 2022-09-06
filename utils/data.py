c1abd3d60f68a09af0cddc8b213120c7d9ecd63a
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2022-09-06 14:22:49 -0400

    fix config

--- utils/data.py
+++ utils/data.py
@@ -13,7 +13,7 @@ def load_config(path):
     with open(path, 'r') as fo:
         config = json.load(fo)
     config = AttrDict(config)
-    print(config.data)
+    print(config.data.dataset_path)
     return config
 
 
