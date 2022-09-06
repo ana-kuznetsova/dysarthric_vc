0f91e1a2471921ce27bce144720b8b7e408a15fe
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2022-09-06 13:36:49 -0400

    fix config

--- utils/data.py
+++ utils/data.py
@@ -6,20 +6,14 @@ import librosa
 import parselmouth
 import json
 
-
-class Struct(dict):
-    def __getattr__(self, name):
-        return self[name]
-
-    def __setattr__(self, name, value):
-        self[name] = value
-
-    def __delattr__(self, name):
-        del self[name]
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
 
 def load_config(path):
     with open(path, 'r') as fo:
-        config = json.load(fo, object_hook=Struct)
+        config = json.load(fo)
+    config = objectview(config)
     return config
 
 
