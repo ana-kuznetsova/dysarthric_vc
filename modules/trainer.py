e305c7a49fba9b2293fab7b5c9e50faeb71d6ab8
Anastasia  Kuznetsova <menina.indigena.17@gmail.com>
2022-09-12 15:57:50 -0400

    training loop

--- modules/trainer.py
+++ modules/trainer.py
@@ -27,11 +27,45 @@ class Trainer():
         model = model.to(device)
 
         if self.config.model.model_name=='speaker_encoder':
-            for batch in train_loader:
-                x = batch['x'].to(device)
-                out = model(x)
-                print("angle proto", out.shape)
+            step = 0
+            prev_val_loss = 0
+            while ep < config.trainer.epoch:
+                print(f"Starting [epoch]:{ep}/{config.trainer.epoch}")
+                for batch in train_loader:
+                    x = batch['x'].to(device)
+                    optimizer.zero_grad()
+                    out = model(x)
+                    out = out.view(config.model.num_speakers, config.model.num_utter, config.model.feat_encoder_dim)
+                    loss = criterion(out)
+                    loss.backward()
+                    scheduler.step()
+                    step+=1
+                    if step%2000==0:
+                        print(f"> Train loss after {step} steps")
+                        print(f'>[Loss]:{loss.data:.5f}')
+                        if config.runner.wandb:
+                            wandb.log({"train_loss": loss})
 
-    
+                ##Validation loop
+                val_loss = 0
+                with torch.no_grad():
+                    for batch in val_loader:
+                        x = batch['x'].to(device)
+                        out = model(x)
+                        out = out.view(config.model.num_speakers, config.model.num_utter, config.model.feat_encoder_dim)
+                        loss = criterion(out)
+                        val_loss+=loss.data
+                val_loss = val_loss/len(val_loader)
+                print(f"> Val loss after {ep} epochs")
+                print(f'> [Loss]:{val_loss:.5f}')
+                if val_loss < prev_val_loss:
+                    #Save checkpoint and lr_sched state
+                    torch.save(model.state_dict(), os.path.join(config.runner.ckpt_path, "best_model.pth"))
+                    torch.save(scheduler.state_dict(), os.path.join(config.runner.ckpt_path, "scheduler.pth"))
+                prev_val_loss = val_loss
+                ep+=1
+                
+
+
     def inference(self):
         pass
